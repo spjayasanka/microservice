72706f94bdd8b4ea5adaae0f1c2b84a30a496da1
spjayasanka <sanjeewapriyanath6@gmail.com>
2024-11-03 20:31:18 +0530

    webhook tst

--- api-gateway/src/main/java/com/tutorial/apigateway/config/JwtUtil.java
+++ api-gateway/src/main/java/com/tutorial/apigateway/config/JwtUtil.java
@@ -11,7 +11,6 @@ import java.util.Base64;
 @Component
 public class JwtUtil {
     private final static String SECRET_KEY = "jdwmrCbaWEskmog3IiOXFoQrmmHDzqFB2ZWNdGHWDng=";
-//    private final static String SECRET_KEY = "jdwmrCbaWEskmog3IiOXFoQrmmHDzqFB2ZWNdGHWDng=";
 
     private final SecretKey key;
 
