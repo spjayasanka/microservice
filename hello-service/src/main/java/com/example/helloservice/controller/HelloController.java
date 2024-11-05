4218a5dafe0f9a03b88443a80b464ecf9cac6aee
spjayasanka <sanjeewapriyanath6@gmail.com>
2024-11-05 22:32:19 +0530

    webhook test 16

--- hello-service/src/main/java/com/example/helloservice/controller/HelloController.java
+++ hello-service/src/main/java/com/example/helloservice/controller/HelloController.java
@@ -19,7 +19,7 @@ public class HelloController {
 
     @GetMapping("/welcome")
     public String testProp() {
-        return "welcome from hello service";
+        return "welcome from hello service fdrthfrth";
     }
 
     @GetMapping("/send-email")
